f709cdb29d273e03aa80debd978c85d5e08f00a2
Aakash  Jaiswal <162143049+aakashjai3209@users.noreply.github.com>
2025-03-22 14:16:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -311,4 +311,6 @@ def add_class():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
